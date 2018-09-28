7ce7e39dd934707f5e57205b489cbc4312abf06a
merkotanh <galina.merkotan@faceit-team.com>
2018-09-28 12:30:54 +0300

    2d commit

--- robot1.rb
+++ robot1.rb
@@ -8,12 +8,11 @@ class Robot
   RIGHT = 'RIGHT'
   LEFT = 'LEFT'
 
-#  @@xedge
-#  @@yedge
+  attr_accessor :direction, :x, :y, :xedge, :yedge
 
-  attr_accessor :direction, :x, :y
-
-  def initialize(faceto = 'north', x = 0, y = 0)
+  def initialize(faceto = 'NORTH', x = 0, y = 0, xedge, yedge)
+    @xedge = xedge
+    @yedge = yedge
     @faceto = faceto
     @x = x
     @y = y
@@ -24,18 +23,24 @@ class Robot
       when 'NORTH'
         ((@y - 1) >= 0) ? @y -= 1 : (p 'CAREFUL! edge')
       when 'EAST'
-      	((@x + 1) <= XEDGE[0]) ? @x += 1 : (p 'CAREFUL! edge')
+      	((@x + 1) <= @xedge) ? @x += 1 : (p 'CAREFUL! edge')
       when 'SOUTH'
-        ((@y + 1) <= XEDGE[0]) ? @y += 1 : (p 'CAREFUL! edge')
+        ((@y + 1) <= @yedge) ? @y += 1 : (p 'CAREFUL! edge')
       when 'WEST'
       	((@x - 1) >= 0) ? @x -= 1 : (p 'CAREFUL! edge')
     end
   end
 
-  def turn(operation, side)
+  def left
     dr = ['NORTH', 'EAST', 'SOUTH', 'WEST']
-    dr.rotate!(2) if @faceto == side
-    @faceto = dr.slice(dr.index(@faceto).send(operation, 1))  
+    dr.rotate!(2) if @faceto == 'NORTH'
+    @faceto = dr.slice(dr.index(@faceto) - 1)
+  end
+
+  def right
+    dr = ['NORTH', 'EAST', 'SOUTH', 'WEST']
+    dr.rotate!(2) if @faceto == 'WEST'
+    @faceto = dr.slice(dr.index(@faceto) + 1)
   end
 
   def place(direction = @faceto, x = @x, y = @y)
@@ -53,28 +58,16 @@ class Robot
     if first_cmd[0] == PLACE 
   	  place(first_cmd[1], first_cmd[2], first_cmd[3])
   	else
-  	  rerun false
+  	  retun false
     end
   end
-
-  def cmds_case(val)
-    case val
-      when 'MOVE' # possible eval (move)??
-        move
-      when 'REPORT'
-        report
-      when 'LEFT'
-      	turn(:-, 'NORTH')
-      when 'RIGHT'
-      	turn(:+, 'WEST')
-      end
-    end
 end
 
-#print "Table width  : "
-#@@xedge = gets.chomp.to_i
-#print "Table height : "
-#@@yedge = gets.chomp.to_i
+print "Table width  : "
+xedge = gets.chomp.to_i
+print "Table height : "
+yedge = gets.chomp.to_i
+
 puts "Robot location with PLACE command."
 puts "Other commands are: LEFT RIGHT MOVE."
 puts "REPORT command specifies Robot's stop."
@@ -85,12 +78,12 @@ begin
   cmds << gets.chomp
 end until cmds.last == 'REPORT'
 
-garik = Robot.new
+garik = Robot.new(xedge, yedge)
 
 if garik.find_location(cmds.first) 
   cmds.each_with_index { |val, index| 
     if index > 0
-      garik.cmds_case(val)
+      eval('garik.'.concat(val.downcase))
     end
   }
 else
